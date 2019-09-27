e6e2c39c7d9d3f52d3b9561bea3bdd6dcd0a612d
Narcis Brindusescu <Narcis75@gmail.com>
2019-09-28 00:16:45 +0300

    update mysql conn

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
       connection: {
         host: '127.0.0.1',
         user: 'root',
-        password: '',
+        password: 'q1w2e3r4',
         database: 'bifa'
     }
     }

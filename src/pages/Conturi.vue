24a23585ca8d1f824d3e66c28aa3dc021703c9f7
Narcis Brindusescu <Narcis75@gmail.com>
2019-09-30 22:50:02 +0300

    algo

--- src/pages/Conturi.vue
+++ src/pages/Conturi.vue
@@ -219,4 +219,24 @@ export default {
 
     }
 }
+
+/*
+function searchTree(element, matchingTitle){
+     if(element.title == matchingTitle){
+          return element;
+     }else if (element.children != null){
+          var i;
+          var result = null;
+          for(i=0; result == null && i < element.children.length; i++){
+               result = searchTree(element.children[i], matchingTitle);
+          }
+          return result;
+     }
+     return null;
+}
+Then you could call it:
+
+var element = data[0];
+var result = searchTree(element, 'randomNode_1');
+*/ 
 </script>

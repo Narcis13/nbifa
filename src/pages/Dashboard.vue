eabb1c27f767af493861aafd16e0adc2b6a4746f
Narcis Brindusescu <Narcis75@gmail.com>
2020-12-27 22:33:13 +0200

    vizionar

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -49,6 +49,67 @@
         <q-btn flat>Rapoarte</q-btn>
       </q-card-actions>
     </q-card>
+
+    <!--  ALOP CARD -->        
+    
+    <q-card class="my-card">
+      <q-card-section>
+       <q-list bordered>
+          <q-expansion-item
+            group="somegroup"
+            icon="explore"
+            label="ALOP"
+            default-opened
+            header-class="text-primary"
+          >
+            <q-card>
+              <q-card-section>
+                  <q-btn class="full-width q-ma-xs" color="indigo" no-caps>
+                    Angajamente<br>Bugetare
+                  </q-btn>
+                  <q-btn class="full-width q-ma-xs" color="indigo" no-caps>
+                     Lichidari<br>Receptii
+                  </q-btn>
+                  <q-btn class="full-width q-ma-xs" color="indigo" no-caps>
+                     Ordonantari<br>Plata
+                  </q-btn>
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+
+          <q-separator />
+
+          <q-expansion-item group="somegroup" icon="perm_identity" label="Nomenclatoare" header-class="text-teal">
+            <q-card>
+              <q-card-section>
+
+                  <q-btn class="full-width q-ma-xs" color="teal" no-caps>
+                    Prevederi<br>Bugetare
+                  </q-btn>
+                  <q-btn class="full-width q-ma-xs" color="teal" no-caps>
+                     Furnizori
+                  </q-btn>
+
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+
+          <q-separator />
+
+          <q-expansion-item group="somegroup" icon="shopping_cart" label="Administrare" header-class="text-purple">
+            <q-card>
+              <q-card-section>
+                 <q-btn class="full-width q-ma-xs" color="purple" no-caps>
+                    Categorii<br>repere
+                  </q-btn>
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+          
+        </q-list>
+      </q-card-section>
+    </q-card>
+
   </div>
 </template>
 

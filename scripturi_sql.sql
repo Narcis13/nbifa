7610d79fad649b278d0872378b108c0df22da963
narcis13 <narcis75@gmail.com>
2021-03-09 14:31:22 +0200

    doar scripturi

--- scripturi_sql.sql
+++ scripturi_sql.sql
@@ -1,7 +1,16 @@
 --
--- Modifica data din cimpul nou data_ang din angajmente cu vaorea corespunzatoare din anteeangajamente
+-- Modifica data din cimpul nou data_ang din angajmente cu valorea corespunzatoare din anteeangajamente
 --
 
 UPDATE angajamente
 INNER JOIN anteteangajamente ON angajamente.idAntet = anteteangajamente.id
 SET angajamente.data_ang = anteteangajamente.dataang
+
+
+--
+-- SElecteaza categoriile cu sume din buget si sume deja angajate
+--
+
+SELECT c.id,c.denumire,c.buget,c.codCap,c.numecap,c.artbug, bug.total as crediteaprobate,@crediteangajate:=ifnull((SELECT sum(suma)  FROM adata.angajamente ang where data_ang>'2021-01-01' and ang.codCap = c.codCap and ang.artbug=c.artbug ),0) as crediteangajate,(bug.total-@crediteangajate) as disponibil  FROM adata.caategorii c
+inner join adata.artbugete bug on bug.id=c.buget
+where c.idcomp=14;

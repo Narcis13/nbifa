e0fc53186e3e9e707992250bc03a632880873eb5
Narcis Brindusescu <Narcis75@gmail.com>
2021-04-22 19:04:34 +0300

    tot furnizori

--- bifalop/src/pages/Furnizori.vue
+++ bifalop/src/pages/Furnizori.vue
@@ -1,7 +1,52 @@
 <template>
-    <div>
-        <h6>Furnizori</h6>
-    </div>
+<q-page padding>
+  <div  class="q-mt-sm flex flex-center">
+    <q-table
+      title="Furnizori"
+      :filter="filter"
+      :rows="state.furnizori"
+      dense
+      :columns="columns"
+      row-key="id"
+      :pagination="initialPagination"
+      selection="single"
+      v-model:selected="selected"
+
+    >
+
+            <template v-slot:top>
+            <div class="q-pa-sm text-h6">Furnizori</div>
+            <div class="row q-gutter-sm q-pa-sm q-ml-xl">
+              
+               <div class="flex" style="min-width:200px;max-height:100px;">
+
+                   <q-btn  class="q-ma-sm"  round color="red" :disable="selected.length==0" icon="delete_forever" >
+                      <q-tooltip class="bg-accent">Sterge</q-tooltip>
+                   </q-btn>
+                  
+               </div>
+
+            </div>
+
+            <q-space />
+            <q-input outlined dense debounce="300" color="primary" v-model="filter">
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+        </template>
+
+
+    </q-table>
+  </div>
+
+    <q-page-sticky  position="bottom-right" :offset="[24, 24]">
+            <q-btn  fab   icon="add" color="accent"  >
+              <q-tooltip anchor="top start" self="center right" class="bg-accent">Adauga Furnizor</q-tooltip>
+            </q-btn>
+    </q-page-sticky>
+
+</q-page>
 </template>
 
 <script >
@@ -10,6 +55,31 @@ import axios from 'axios'
 import { date } from 'quasar'
 import { useQuasar } from 'quasar'
 
+const columns = [
+
+  { name: 'denumire', align: 'left', label: 'Furnizor', field: 'denumire', sortable: true },
+  { name: 'codfiscal', align: 'left', label: 'Cod fiscal', field: 'codfiscal', sortable: true },
+  { name: 'iban', align: 'left',label: 'Cont IBAN', field: 'iban', sortable: false },
+  { name: 'adresa', align: 'left',label: 'Adresa', field: 'adresa', sortable: false },
+  { name: 'localitate', align: 'left',label: 'Localitate', field: 'localitate', sortable: true },
+  { name: 'judet', align: 'left',label: 'Judet', field: 'judet', sortable: true },
+  /*{ name: 'artbug', align: 'center',label: 'Art. bug.', field: 'artbug', sortable: true },
+  { name: 'viza', align: 'center',label: 'Viza CFPP', field: 'viza', sortable: true }*/
+ 
+]
+
+const  initialPagination = {
+       // sortBy: 'desc',
+       // descending: false,
+        page: 1,
+        rowsPerPage: 15
+
+      }
+const state = reactive(
+  {
+    furnizori : []  
+  }
+  )
 export default defineComponent({
     setup () {
             const $q = useQuasar()
@@ -25,7 +95,10 @@ export default defineComponent({
 
                     res => {
                     console.log('Raspuns la toate angajamentele',res.data);
-                    
+                    state.furnizori=[];
+                    res.data.furnizori.map(f=>{
+                        state.furnizori.push(f)
+                    })
                 
                     }
                 ).catch(err =>{})
@@ -33,7 +106,17 @@ export default defineComponent({
 
         totiFurnizorii();
 
-        return {}
+
+        let selected= ref([])
+
+
+        return {
+            state,
+            initialPagination,
+            columns,
+            selected,
+            filter:ref(''),
+            }
     }
 })
 </script>
--- server/api/controllers/furn.js
+++ server/api/controllers/furn.js
@@ -3,8 +3,14 @@ const knex =require('../../dbaky.js');
 module.exports.toti = (req,res,next)=> {
 
   console.log('Toti furnizorii compartimentului...',req.params.idcomp)
-  return res.status(200).json({
-    message: "toti furnizorii"
-  });
+
+  knex('furnizori').where('addedby',req.params.idcomp).orderBy('id', 'desc').then(r=>{
+
+    return res.status(200).json({
+      message: "toti furnizorii",
+      furnizori:r
+    });
+  }).catch(err =>{console.log(err)})
+
 
 }

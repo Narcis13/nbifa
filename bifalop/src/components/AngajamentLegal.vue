f57832d307912ae2b8c7dd3cfc19edea4635efe1
Narcis Brindusescu <Narcis75@gmail.com>
2021-07-05 19:07:03 +0300

    reluam

--- bifalop/src/components/AngajamentLegal.vue
+++ bifalop/src/components/AngajamentLegal.vue
@@ -15,8 +15,29 @@
       <q-tab-panels v-model="panelActiv" animated class="shadow-2 rounded-borders">
         <q-tab-panel name="unul">
           <div class="text-subtitle1">Ang. bugetar nr. {{angBugetar.nrdoc}} din {{angBugetar.dataang}} - {{angBugetar.detalii}}</div>
-            <div class="text-subtitle2"> Categoria: {{angBugetar.categorie}} Art. bug. {{angBugetar.artbug}} <span class="text-right">Suma disponibila: {{angBugetar.suma}}</span></div>
-          
+          <div class="text-subtitle2"> Categoria: {{angBugetar.categorie}} Art. bug. {{angBugetar.artbug}} <span class="text-right">Suma disponibila: {{angBugetar.suma}}</span></div>
+           <div class="q-gutter-md row">
+              <q-select
+                filled
+                v-model="furnizor"
+                use-input
+                hide-selected
+                fill-input
+                input-debounce="0"
+                :options="furnizori"
+                @filter="filterFn"
+                hint="Minimum 2 caractere pentru a cauta..."
+                style="width: 250px; padding-bottom: 32px"
+              >
+                <template v-slot:no-option>
+                  <q-item>
+                    <q-item-section class="text-grey">
+                      No results
+                    </q-item-section>
+                  </q-item>
+                </template>
+              </q-select>
+          </div>
         </q-tab-panel>
 
         <q-tab-panel name="toate">
@@ -37,17 +58,34 @@ import { date } from 'quasar'
 import axios from 'axios'
 import { useQuasar } from 'quasar'
 
-
+const totiFurnizorii = [
+  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+]
 
 export default defineComponent({
     name:'AngajamentLegal',
     props:['ang_bugetar'],
     setup (props, { emit }) {
        console.log('Prop ang legal',props.ang_bugetar)
+       let furnizori=ref([])
+       let furnizor = ref(null)
        let panelActiv = ref('unul')
         return {
             panelActiv,
+            furnizori,
+            furnizor,
             angBugetar:props.ang_bugetar,
+            filterFn (val, update, abort) {
+                if (val.length < 2) {
+                  abort()
+                  return
+                }
+
+                update(() => {
+                  const needle = val.toLowerCase()
+                  furnizori.value = totiFurnizorii.filter(v => v.toLowerCase().indexOf(needle) > -1)
+                })
+          },
             schimbaPanelActiv(){
                if(panelActiv.value=='unul')
                   panelActiv.value='toate'

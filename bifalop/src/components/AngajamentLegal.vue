2cb581d5f972ade20ebdb3988331a5d5d47172cd
narcis13 <narcis75@gmail.com>
2021-08-09 13:14:47 +0300

    greeeeeuuuu

--- bifalop/src/components/AngajamentLegal.vue
+++ bifalop/src/components/AngajamentLegal.vue
@@ -71,8 +71,40 @@
         </q-tab-panel>
 
         <q-tab-panel name="toate">
-          <div class="text-h6">Toate ang. legale</div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <div class="q-pa-md">
+              <q-table
+               
+                dense
+                :filter="filter"
+                :pagination="initialPagination"
+                :rows="rows"
+                v-model:selected="selected"
+                :columns="columns"
+                row-key="name"
+                selection="single"
+              >
+
+                  <template v-slot:top>
+
+                      <div class="flex" style="min-width:200px;max-height:100px;">
+  
+                          <q-btn  class="q-ma-sm"  round color="red" icon="delete_forever" >
+                              <q-tooltip class="bg-accent">Sterge</q-tooltip>
+                          </q-btn>
+
+                      </div>
+
+                
+
+                    <q-space />
+                    <q-input outlined dense debounce="300" color="primary" v-model="filter">
+                      <template v-slot:append>
+                        <q-icon name="search" />
+                      </template>
+                    </q-input>
+                </template>
+              </q-table>
+            </div>
         </q-tab-panel>
 
       </q-tab-panels>
@@ -87,6 +119,137 @@ import { defineComponent,ref,computed,inject } from 'vue'
 import { date } from 'quasar'
 import axios from 'axios'
 import { useQuasar } from 'quasar'
+
+const  initialPagination = {
+       // sortBy: 'desc',
+       // descending: false,
+        page: 1,
+        rowsPerPage: 10
+
+      }
+
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'protein', label: 'Protein (g)', field: 'protein' },
+  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+  { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+const rows = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: '14%',
+    iron: '1%'
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: '8%',
+    iron: '1%'
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: '6%',
+    iron: '7%'
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    sodium: 413,
+    calcium: '3%',
+    iron: '8%'
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356,
+    fat: 16.0,
+    carbs: 49,
+    protein: 3.9,
+    sodium: 327,
+    calcium: '7%',
+    iron: '16%'
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375,
+    fat: 0.0,
+    carbs: 94,
+    protein: 0.0,
+    sodium: 50,
+    calcium: '0%',
+    iron: '0%'
+  },
+  {
+    name: 'Lollipop',
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    sodium: 38,
+    calcium: '0%',
+    iron: '2%'
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    sodium: 562,
+    calcium: '0%',
+    iron: '45%'
+  },
+  {
+    name: 'Donut',
+    calories: 452,
+    fat: 25.0,
+    carbs: 51,
+    protein: 4.9,
+    sodium: 326,
+    calcium: '2%',
+    iron: '22%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  }
+]
+
 
 let totiFurnizorii = [
 
@@ -134,6 +297,11 @@ export default defineComponent({
             nrcontract,
             valoare,
             deladata,
+            columns,
+            rows,
+            selected: ref([]),
+            filter:ref(''),
+            initialPagination,
             angBugetar:props.ang_bugetar,
             filterFn (val, update, abort) {
                 if (val.length < 2) {
